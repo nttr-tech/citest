c04d90ee71dbe43aed7e31162e9a6446624d03ed
Toshiya KURISHIMA <kuribone@gmail.com>
2015-04-20 12:21:32 +0900

    test

--- src/main/java/jp/co/nttr_tech/citest/Account.java
+++ src/main/java/jp/co/nttr_tech/citest/Account.java
@@ -3,6 +3,9 @@ package jp.co.nttr_tech.citest;
 public class Account {
 
 	public boolean login(String userName, String passWord) {
+		if("001".equals(userName)){
+			return true;
+			}
 		return false;
 	}
 
