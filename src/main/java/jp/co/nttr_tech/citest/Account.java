2bd43deb7b6e006a3e70eec57d42c81c0c23dfd9
TSUNODA Kazuya <tsunoda@nttr-tech.co.jp>
2015-04-24 15:58:22 +0900

    is/pwのバリデーション追加

--- src/main/java/jp/co/nttr_tech/citest/Account.java
+++ src/main/java/jp/co/nttr_tech/citest/Account.java
@@ -2,10 +2,16 @@ package jp.co.nttr_tech.citest;
 
 public class Account {
 
-	public boolean login(String userName, String passWord) {
-		if("001".equals(userName)){
+	public boolean login(String userName, String userPassword) {
+//		if(!ValidateForm.isValidString(userName)){
+//			return false;
+//		}
+//		if(!ValidateForm.isValidPassword(userName, userPassword)){
+//			return false;
+//		}		
+		if("001".equals(userName) && "002".equals(userPassword)){
 			return true;
-			}
+		}
 		return false;
 	}
 
