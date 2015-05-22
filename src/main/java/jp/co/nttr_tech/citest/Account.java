326276a992955ab8611493f6e6855ff80821bc12
TSUNODA Kazuya <tsunoda@nttr-tech.co.jp>
2015-05-22 14:32:56 +0900

    demo初期状態

--- src/main/java/jp/co/nttr_tech/citest/Account.java
+++ src/main/java/jp/co/nttr_tech/citest/Account.java
@@ -3,12 +3,6 @@ package jp.co.nttr_tech.citest;
 public class Account {
 
 	public boolean login(String userName, String userPassword) {
-//		if(!ValidateForm.isValidString(userName)){
-//			return false;
-//		}
-//		if(!ValidateForm.isValidPassword(userName, userPassword)){
-//			return false;
-//		}		
 		if("001".equals(userName) && "002".equals(userPassword)){
 			return true;
 		}
